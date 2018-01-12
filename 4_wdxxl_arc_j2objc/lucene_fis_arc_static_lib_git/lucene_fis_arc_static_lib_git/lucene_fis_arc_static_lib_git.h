bd1058535383ef4d7d5930a5fabd457df9e7b668
wdxxl <wdxxlanswer@gmail.com>
2018-01-12 13:41:52 +0800

    remove lucene and add fis_arc lucene static lib project

---
+++ 4_wdxxl_arc_j2objc/lucene_fis_arc_static_lib_git/lucene_fis_arc_static_lib_git/lucene_fis_arc_static_lib_git.h
@@ -0,0 +1,13 @@
+//
+//  lucene_fis_arc_static_lib_git.h
+//  lucene_fis_arc_static_lib_git
+//
+//  Created by 王科学 on 12/01/2018.
+//  Copyright © 2018 lucene_fis_arc_static_lib_git. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface lucene_fis_arc_static_lib_git : NSObject
+
+@end
--- lucene/README.md
+++
@@ -1,7 +0,0 @@
-lucene-core & lucene-grouping 3.5.0
-
-
-
-org.apache.lucene.analysis.CharStream.java -> org.apache.lucene.analysis.CharStreamAnalysis.java
-org.apache.lucene.analysis.Token -> org.apache.lucene.analysis.TokenAnalysis
-
