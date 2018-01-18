ba2aa29b2bde5ceff90b5eca8af9b5e8d6f8178b
wdxxl <wdxxlanswer@gmail.com>
2018-01-18 16:09:19 +0800

    add demo TSM and FRMFIM.

--- cordova_lucene_noarc/platforms/ios/platform_www/plugins/com.lucene.noarc/www/lucene_plugin.js
+++ cordova_lucene_noarc/platforms/ios/platform_www/plugins/com.lucene.noarc/www/lucene_plugin.js
@@ -5,4 +5,8 @@ exports.coolMethod = function(arg0, success, error) {
     exec(success, error, "lucene_plugin", "coolMethod", [arg0]);
 };
 
+exports.coolMethodTSM = function(arg0, success, error) {
+    exec(success, error, "lucene_plugin", "coolMethodTSM", [arg0]);
+};
+
 });
--- cordova_lucene_noarc/plugins/com.lucene.noarc/www/lucene_plugin.js
+++ cordova_lucene_noarc/plugins/com.lucene.noarc/www/lucene_plugin.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.coolMethod = function(arg0, success, error) {
     exec(success, error, "lucene_plugin", "coolMethod", [arg0]);
 };
+
+exports.coolMethodTSM = function(arg0, success, error) {
+    exec(success, error, "lucene_plugin", "coolMethodTSM", [arg0]);
+};
